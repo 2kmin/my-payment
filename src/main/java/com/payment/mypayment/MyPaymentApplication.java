5e73553285eb21a19849faf47cff573716954ac9
kangmin.lee <comes.009@kurlycorp.com>
2023-04-24 11:18:56 +0900

    first commit

---
+++ src/main/java/com/payment/mypayment/MyPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.payment.mypayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyPaymentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/payment/mypayment/MyPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.mypayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPaymentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
