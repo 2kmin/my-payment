fff7d2b7a1849b669821d4fde4fea2ffe7be3905
kangmin.lee <comes.009@kurlycorp.com>
2023-04-25 18:34:05 +0900

    결제 생성 API 추가 및 파라미터 검증

---
+++ src/main/java/com/payment/mypayment/exception/ValidationException.java
@@ -0,0 +1,25 @@
+package com.payment.mypayment.exception;
+
+import com.payment.mypayment.common.type.ResponseType;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
+@Getter
+@Slf4j
+public class ValidationException extends RuntimeException{
+
+    private ResponseType error;
+    private String detailMessage;
+
+    public ValidationException(String message){
+        super(message);
+    }
+
+    public ValidationException(ResponseType error, String detailMessage ) {
+        this.error = error;
+        this.detailMessage = detailMessage;
+    }
+}
